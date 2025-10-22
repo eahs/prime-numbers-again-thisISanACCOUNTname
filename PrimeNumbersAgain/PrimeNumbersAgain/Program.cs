94e6280b2ab49ea0c48329dd23bb5d6ce236f8f1
Joseph Trilli <rebooteed@gmail.com>
2025-10-22 10:35:43 -0400

    working 2 million
    and optimizations

--- PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
+++ PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace PrimeNumbersAgain
@@ -18,54 +17,73 @@ namespace PrimeNumbersAgain
             timer.Start();
             prime = FindNthPrime(n);
             timer.Stop();
-            
-            
-            Console.WriteLine($"\nToo easy.. {prime} is the nth prime when n is {n}. I found that answer in {timer.Elapsed.Seconds} seconds.");
 
-            EvaluatePassingTime(timer.Elapsed.Seconds);
+            Console.WriteLine($"\nToo easy.. {prime} is the nth prime when n is {n}. I found that answer in {timer.Elapsed.TotalSeconds:F3} seconds.");
+
+            EvaluatePassingTime(timer.Elapsed);
         }
 
         static int FindNthPrime(int n)
         {
-            // the sieve of eratosthenes, with built in exceptions
-
-            if (n == 1)
+            if (n < 1)
             {
-                return 2;
+                throw new ArgumentException("n must be >= 1", nameof(n));
             }
 
-            if (n < 2)
+            // quick returns for small n
+            if (n == 1) return 2;
+            if (n == 2) return 3;
+            if (n == 3) return 5;
+            if (n == 4) return 7;
+            if (n == 5) return 11;
+
+            // estimate an upper bound for the nth prime using p_n < n (ln n + ln ln n) for n >= 6
+            double dn = n;
+            int limit;
+            try
             {
-                return 3;
+                double estimate = dn * (Math.Log(dn) + Math.Log(Math.Log(dn)));
+                limit = (int)(estimate + 10);
+            }
+            catch
+            {
+                limit = 2000000; // fallback
             }
 
+            // ensure minimum size
+            if (limit < 100) limit = 100;
 
-            var limit = 2000000; // upper limit to find primes
-            bool[] isPrime = new bool[limit + 1];
-            for (int i = 2; i <= limit; i++) 
-            { 
-                isPrime[i] = true;
-            }   
-            for (int i = 2; i * i <= limit; i++)
+            // sieve of eratosthenes
+            while (true)
+            {
+                bool[] isPrime = new bool[limit + 1];
+                for (int i = 2; i <= limit; i++)
                 {
-                if (isPrime[i])
+                    isPrime[i] = true;
+                }
+
+                for (int i = 2; i * i <= limit; i++)
                 {
-                    for (int j = i * i; j <= limit; j += i)
+                    if (isPrime[i])
                     {
-                        isPrime[j] = false;
+                        for (int j = i * i; j <= limit; j += i)
+                        {
+                            isPrime[j] = false;
+                        }
                     }
                 }
-            }
-            List<int> primes = new List<int>();
-            for (int i = 2; i <= limit; i++)
-            {
-                if (isPrime[i])
+
+                List<int> primes = new List<int>();
+                for (int i = 2; i <= limit; i++)
                 {
-                    primes.Add(i);
+                    if (isPrime[i]) primes.Add(i);
+                }
+
+                if (primes.Count >= n)
+                {
+                    return primes[n - 1];
                 }
             }
-            return primes[n - 1]; // return the nth prime (1-based index)
-
         }
 
         static int GetNumber()
@@ -74,11 +92,13 @@ namespace PrimeNumbersAgain
             while (true)
             {
                 Console.Write("Which nth prime should I find?: ");
-                
+
                 string num = Console.ReadLine();
                 if (Int32.TryParse(num, out n))
                 {
-                    return n;
+                    if (n >= 1) return n;
+                    Console.WriteLine($"{num} is not a valid number. Please enter an integer greater than or equal to 1.\n");
+                    continue;
                 }
 
                 Console.WriteLine($"{num} is not a valid number.  Please try again.\n");
@@ -95,15 +115,15 @@ namespace PrimeNumbersAgain
             Console.WriteLine(".##......##..##..######..##...##..######...####..");
             Console.WriteLine(".................................................\n\n");
             Console.WriteLine("Nth Prime Solver O-Matic Online..\nGuaranteed to find primes up to 2 million in under 30 seconds!\n\n");
-            
+
         }
 
-        static void EvaluatePassingTime(int time)
+        static void EvaluatePassingTime(TimeSpan elapsed)
         {
             Console.WriteLine("\n");
             Console.Write("Time Check: ");
 
-            if (time <= 3)
+            if (elapsed.TotalSeconds <= 3)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Pass");
@@ -113,9 +133,6 @@ namespace PrimeNumbersAgain
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Fail");
             }
-
-            Console.ForegroundColor = ConsoleColor.Gray;
-            
         }
     }
 }
